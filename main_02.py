82cb65ec83919eba40c8c8f07b0537f606a428d8
Sumerk-2024 <163854268+Sumerk-2024@users.noreply.github.com>
2024-07-13 23:57:46 +0300

    add file main.py
    Добавить файл main.py.

---
+++ main_02.py
@@ -0,0 +1,5 @@
+def check(number):
+   return number % 2 == 0
+
+
+
---
+++ test-03.py
@@ -0,0 +1,17 @@
+# import pytest
+# from main_03 import is_palindrome
+#
+#
+# def test_is_palindrome():
+#     assert is_palindrome('madam') == True
+#     assert is_palindrome('hello') == False
+#
+#
+# @pytest.mark.parametrize("test_input,expected", [
+#     ('level', True),
+#     ('python', False),
+#     ('racecar', True),
+#     ('', True),
+# ])
+# def test_is_palindrome(test_input, expected):
+#     assert is_palindrome(test_input) == expected
